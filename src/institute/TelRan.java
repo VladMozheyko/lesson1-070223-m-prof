eab6e90578a85b6526086b7d35097b96ff6e95e2
PI20-4-Baronkin <git13509!>
2023-05-09 15:35:40 +0300

    Initial commit

---
+++ src/institute/TelRan.java
@@ -0,0 +1,38 @@
+package institute;
+
+import java.util.Scanner;
+
+public class TelRan {
+    private Scanner scanner = new Scanner(System.in);
+    private Pupil[] pupils = new Pupil[3];
+    private Teacher teacher;
+    private Worker worker;
+
+    /**
+     * Метод для инициализации членов организации
+     */
+    public void init(){
+        System.out.println("Введите данные для преподавателя: возраст, имя, квалификация");
+        teacher = new Teacher(scanner.nextInt(), scanner.next(), scanner.next());
+        System.out.println("Введите данные для работника: возраст, имя, должность");
+        worker = new Worker(scanner.nextInt(), scanner.next(), scanner.next());
+        for (int i = 0; i < pupils.length; i++) {
+            System.out.println("Введите данные для ученика: возраст, имя, направление");
+            pupils[i] = new Pupil(scanner.nextInt(), scanner.next(), scanner.next());
+        }
+    }
+
+    public void performLection(){
+        System.out.println("Введите название лекции");
+        String lection = scanner.next();
+        teacher.doWork("ведет лекцию по " + lection);
+        for (int i = 0; i < pupils.length; i++) {
+            pupils[i].doWork("посещает лекцию по " + lection);
+        }
+        worker.doWork("оплачивает лекцию по " + lection);
+    }
+
+
+
+
+}
